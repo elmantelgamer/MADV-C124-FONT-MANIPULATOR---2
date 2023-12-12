10fc79c5894428932f0b1ce0e4e0cd8ffbfda64f
elmantelgamer <115519343+elmantelgamer@users.noreply.github.com>
2023-12-11 20:15:40 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,500);
+    canvas.position(560,150);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes)
+}
+function modelLoaded(){
+    console.log("poseNet is initialized");
+
+
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
